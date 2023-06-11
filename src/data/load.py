e147ba3c8cfe653477925669540dec8e39214167
David <dmorara@unal.edu.co>
2023-06-11 14:33:20 -0500

    reformat load.py

--- src/data/load.py
+++ src/data/load.py
@@ -6,26 +6,36 @@ import argparse
 import wandb
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--IdExecution', type=str, help='ID of the execution')
+parser.add_argument("--IdExecution", type=str, help="ID of the execution")
 args = parser.parse_args()
 
 if args.IdExecution:
     print(f"IdExecution: {args.IdExecution}")
 
-def load(train_size=.8):
+
+def load(train_size=0.8):
     """
     # Load the data
     """
-      
-    # the data, split between train and test sets
-    train = torchvision.datasets.MNIST(root='./data', train=True, download=True)
-    test = torchvision.datasets.MNIST(root='./data', train=False, download=True)
 
-    (x_train, y_train), (x_test, y_test) = (train.data, train.targets), (test.data, test.targets)
+    # the data, split between train and test sets
+    train = torchvision.datasets.MNIST(root="./data", train=True, download=True)
+    test = torchvision.datasets.MNIST(root="./data", train=False, download=True)
+
+    (x_train, y_train), (x_test, y_test) = (train.data, train.targets), (
+        test.data,
+        test.targets,
+    )
 
     # split off a validation set for hyperparameter tuning
-    x_train, x_val = x_train[:int(len(train)*train_size)], x_train[int(len(train)*train_size):]
-    y_train, y_val = y_train[:int(len(train)*train_size)], y_train[int(len(train)*train_size):]
+    x_train, x_val = (
+        x_train[: int(len(train) * train_size)],
+        x_train[int(len(train) * train_size) :],
+    )
+    y_train, y_val = (
+        y_train[: int(len(train) * train_size)],
+        y_train[int(len(train) * train_size) :],
+    )
 
     training_set = TensorDataset(x_train, y_train)
     validation_set = TensorDataset(x_val, y_val)
@@ -33,19 +43,27 @@ def load(train_size=.8):
     datasets = [training_set, validation_set, test_set]
     return datasets
 
+
 def load_and_log():
     # 🚀 start a run, with a type to label it and a project it can call home
-    with wandb.init(project="MLOps-Pycon2023",name=f"Load Raw Data ExecId-{args.IdExecution}", job_type="load-data") as run:
-        
+    with wandb.init(
+        project="MLOps-Pycon2023",
+        name=f"Load Raw Data ExecId-{args.IdExecution}",
+        job_type="load-data",
+    ) as run:
         datasets = load()  # separate code for loading the datasets
         names = ["training", "validation", "test"]
 
         # 🏺 create our Artifact
         raw_data = wandb.Artifact(
-            "mnist-raw", type="dataset",
+            "mnist-raw",
+            type="dataset",
             description="raw MNIST dataset, split into train/val/test",
-            metadata={"source": "torchvision.datasets.MNIST",
-                      "sizes": [len(dataset) for dataset in datasets]})
+            metadata={
+                "source": "torchvision.datasets.MNIST",
+                "sizes": [len(dataset) for dataset in datasets],
+            },
+        )
 
         for name, data in zip(names, datasets):
             # 🐣 Store a new file in the artifact, and write something into its contents.
@@ -55,5 +73,6 @@ def load_and_log():
 
         # ✍️ Save the artifact to W&B.
         run.log_artifact(raw_data)
+
 
 load_and_log()
